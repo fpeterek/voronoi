ce3ab8b7b7c9028fe68e5a980804932ec7f5f888
Filip Peterek <fpeterek@seznam.cz>
2019-12-22 23:18:20 +0100

    Manhattan distance

--- src/pivot.cpp
+++ src/pivot.cpp
@@ -15,7 +15,7 @@ uint64_t Pivot::distanceFrom(const int64_t otherX, const int64_t otherY) const {
     const int64_t distX = otherX - x;
     const int64_t distY = otherY - y;
 
-    return std::sqrt(distX*distX + distY*distY);
+    return distanceFunction(distX, distY);
 
 }
 
@@ -28,4 +28,18 @@ bool Pivot::operator==(const Pivot & other) const {
 
 bool Pivot::operator!=(const Pivot & other) const {
     return not (operator==(other));
+}
+
+Pivot::DistanceFunction Pivot::distanceFunction = Pivot::euclideanDistance;
+
+int64_t Pivot::manhattanDistance(int64_t distX, int64_t distY) {
+    return std::abs(distX) + std::abs(distY);
+}
+
+int64_t Pivot::euclideanDistance(int64_t distX, int64_t distY) {
+    return std::sqrt(distX*distX + distY*distY);
+}
+
+void Pivot::switchDistanceFunction() {
+    distanceFunction = Pivot::manhattanDistance;
 }
